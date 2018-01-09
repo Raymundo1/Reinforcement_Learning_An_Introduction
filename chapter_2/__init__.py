f1d8faab04c600357e22b513dee4371d48c3141e
Raymundo1 <cquptcxl@gmail.com>
2018-01-08 18:38:09 -0700

    finish baisc structure

---
+++ chapter_2/__init__.py
@@ -0,0 +1,5 @@
+#######################################################################
+# Copyright (C) 2018 Xinlei Chen(cquptcxl@gmail.com)                  #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
